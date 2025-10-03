7a28533ace3eb201dad18508a84bddccd4dd7c66
Abinav-Ramjee123 <aramjee@uoguelph.ca>
2025-10-03 12:21:09 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -3,11 +3,35 @@
 # Word frequency exercise
 # TODO: (Read detailed instructions in the Readme file)
 # 1. Prompt the user: Ask the user to enter a sentence.
+user_sentence = input("enter a sentence: ")
+while is_sentence(user_sentence) == False:
+    print("this does not meet the criteria for a sentence")
+    user_sentence = input("enter a sentence: ")
+    
+
 # 2. Split the sentence
 # 3. Create lists to store words and their corresponding frequencies.
 # 4. Iterate through words and update frequencies
 
 import re
+
+words = re.findall(r'\b\w+\b', user_sentence.lower())
+
+unique_words = []
+frequencies = []
+
+
+for word in words:
+    if word in unique_words:
+        index = unique_words.index(word)
+        frequencies[index] += 1
+    else:
+        unique_words.append(word)
+        frequencies.append(1)
+
+print("\nWord Frequencies:")
+for i in range(len(unique_words)):
+    print(unique_words[i] + ":", frequencies[i])
 
 #This is a function that checks if a text qualifies as a sentence. You do not need to modify this!
 def is_sentence(text):
